527189fc531fa848ad22de01afd43a8f54b9f977
Jason MacWilliams <jason@discoverygarden.ca>
2012-09-20 11:39:51 -0300

    simple islandora module supplying an interface between the jwplayer and the islandora audio solution pack

---
+++ js/player.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+
+  jwplayer("mediaplayer").setup({
+    flashplayer: Drupal.settings.islandora_jwplayer.jwpath + "/player.swf",
+    file: Drupal.settings.islandora_jwplayer.url,
+//    type: "audio", // forced to audio mode
+    type: Drupal.settings.islandora_jwplayer.mimetype.split("/")[0], // automatic audio/video detection
+    image: Drupal.settings.islandora_jwplayer.thumbnail,
+    controlbar: "bottom"
+  });
+
+});
